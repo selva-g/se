ec29af8db6b66f812ff92ad371e1a2f7e32116f3
selva-g <selvagvinayak@gmail.com>
2020-11-11 10:12:34 +0530

    added keyword search field and groups category

--- script.js
+++ script.js
@@ -62,3 +62,132 @@ $(document).ready(function() {
    $(".hid").css("display", "block");
     //console.log("alerting");
   });
+
+
+  //Group drop down
+  function checkboxDropdown(el) {
+    var $el = $(el)
+  
+    function updateStatus(label, result) {
+      if(!result.length) {
+        label.html('Select Options');
+      }
+    };
+    
+    $el.each(function(i, element) {
+      var $list = $(this).find('.dropdown-list'),
+        $label = $(this).find('.dropdown-label'),
+        $checkAll = $(this).find('.check-all'),
+        $inputs = $(this).find('.check'),
+        defaultChecked = $(this).find('input[type=checkbox]:checked'),
+        result = [];
+      
+      updateStatus($label, result);
+      if(defaultChecked.length) {
+        defaultChecked.each(function () {
+          result.push($(this).next().text());
+          $label.html(result.join(", "));
+        });
+      }
+      
+      $label.on('click', ()=> {
+        $(this).toggleClass('open');
+      });
+  
+      $checkAll.on('change', function() {
+        var checked = $(this).is(':checked');
+        var checkedText = $(this).next().text();
+        result = [];
+        if(checked) {
+          result.push(checkedText);
+          $label.html(result);
+          $inputs.prop('checked', false);
+        }else{
+          $label.html(result);
+        }
+          updateStatus($label, result);
+      });
+  
+      $inputs.on('change', function() {
+        var checked = $(this).is(':checked');
+        var checkedText = $(this).next().text();
+        if($checkAll.is(':checked')) {
+          result = [];
+        }
+        if(checked) {
+          result.push(checkedText);
+          $label.html(result.join(", "));
+          $checkAll.prop('checked', false);
+        }else{
+          let index = result.indexOf(checkedText);
+          if (index >= 0) {
+            result.splice(index, 1);
+          }
+          $label.html(result.join(", "));
+        }
+        updateStatus($label, result);
+      });
+  
+      $(document).on('click touchstart', e => {
+        if(!$(e.target).closest($(this)).length) {
+          $(this).removeClass('open');
+        }
+      });
+    });
+  };
+  
+  checkboxDropdown('.dropdown');
+  
+  
+
+  //End of Group drop down
+
+
+  //authorkeyword
+  var textRemove = new Choices(document.getElementById('choices-text-remove-button'), {
+    delimiter: ',',
+    editItems: true,
+    maxItemCount: 5,
+    removeItemButton: true,
+  });
+ var multipleCancelButton = new Choices('#choices-multiple-remove-button', {
+   removeItemButton: true,
+ });
+ var customTemplates = new Choices(document.getElementById('choices-single-custom-templates'), {
+         callbackOnCreateTemplates: function(strToEl) {
+           var classNames = this.config.classNames;
+           var itemSelectText = this.config.itemSelectText;
+           return {
+             item: function(classNames, data) {
+               return strToEl('\
+                 <div\
+                   class="'+ String(classNames.item) + ' ' + String(data.highlighted ? classNames.highlightedState : classNames.itemSelectable) + '"\
+                   data-item\
+                   data-id="'+ String(data.id) + '"\
+                   data-value="'+ String(data.value) + '"\
+                   '+ String(data.active ? 'aria-selected="true"' : '') + '\
+                   '+ String(data.disabled ? 'aria-disabled="true"' : '') + '\
+                   >\
+                   <span style="margin-right:10px;">🎉</span> ' + String(data.label) + '\
+                 </div>\
+               ');
+             },
+             choice: function(classNames, data) {
+               return strToEl('\
+                 <div\
+                   class="'+ String(classNames.item) + ' ' + String(classNames.itemChoice) + ' ' + String(data.disabled ? classNames.itemDisabled : classNames.itemSelectable) + '"\
+                   data-select-text="'+ String(itemSelectText) + '"\
+                   data-choice \
+                   '+ String(data.disabled ? 'data-choice-disabled aria-disabled="true"' : 'data-choice-selectable') + '\
+                   data-id="'+ String(data.id) + '"\
+                   data-value="'+ String(data.value) + '"\
+                   '+ String(data.groupId > 0 ? 'role="treeitem"' : 'role="option"') + '\
+                   >\
+                   <span style="margin-right:10px;">👉🏽</span> ' + String(data.label) + '\
+                 </div>\
+               ');
+             },
+           };
+         }
+       });
+//end of authorkeyword 
