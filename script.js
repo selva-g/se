52150d511afcb8187715a62844e0343df1a49a9c
selva-g <selvagvinayak@gmail.com>
2020-11-11 17:13:09 +0530

    added the groups and keyword sections in mobile view

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ $(document).ready(function() {
 
   /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
   function openNav() {
-	document.getElementById("mySidenav").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("mySidenav").style.width = "350px";
+	document.getElementById("main").style.marginLeft = "350px";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
